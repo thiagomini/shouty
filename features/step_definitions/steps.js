73dd8e7562d6f4d5fc464265aa13bb8fed5707c1
Thiago Martins <thiago_valentim1@hotmail.com>
2022-12-08 14:52:33 -0300

    feat: creat first feature and stub steps

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,16 @@
+const { Given, Then, When } = require("@cucumber/cucumber");
+
+Given("Lucy is located {int} metres from Sean", function (distance) {
+  console.log(distance);
+  return "pending";
+});
+
+When("Sean shouts {string}", function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("Lucy hears Sean's message", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
